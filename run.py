b6ba15e404d892c0f3313c4096033450d22ad4d8
tomasblomgren <Tomas.blomgren96@gmail.com>
2022-07-18 08:55:57 +0000

    retrieving latest numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -26,7 +27,7 @@ def get_sales_data():
         data_str = input("Enter your numbers here!: ")
 
         sales_data = data_str.split(",")
-      
+
         if validate_data(sales_data):
             print("Data is valid!")
             break
@@ -61,6 +62,27 @@ def update_sales_worksheet(data):
     print("sales worksheet updated successfully.\n")
 
 
-data = get_sales_data()
-sales_data = [int(num)for num in data]
-update_sales_worksheet(sales_data)
+def calculate_surplus_data(sales_row):
+    """
+    compare sales with stock and calculate surplus data
+    """
+
+
+print("calulating surplus data...\n")
+stock = SHEET.worksheet("stock").get_all_values()
+stock_row = stock[-1]
+print(stock_row)
+
+
+def main():
+    """
+    Run all program funtions
+    """
+    data = get_sales_data()
+    sales_data = [int(num)for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to love sandwiches data automation")
+main()
